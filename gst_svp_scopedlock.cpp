157ec08d4e686e5e87281debcafa51ebeaeee428
Douglas Adler <douglas_adler2@comcast.com>
2022-03-18 21:11:19 +0000

    LLAMA-5776 : Change performance reporting methods to use trylock
    Reason for change: Performance functions get locked up if the gst-svp-ext
    mutex is locked.  This will cause the hang detector to kill the process.
    Test Procedure: Play OTT video and tune to linear channels
    
    Risks: Low
    
    Signed-off-by: Douglas Adler <douglas_adler2@comcast.com>
    Change-Id: I061a8eec413acf0344b94cd134417be471132c2e

--- gst_svp_scopedlock.cpp
+++ gst_svp_scopedlock.cpp
@@ -1,20 +1,25 @@
-/*
- * If not stated otherwise in this file or this component's LICENSE file the
- * following copyright and licenses apply:
- *
- * Copyright 2021 RDK Management
- *
- * Licensed under the Apache License, Version 2.0 (the License);
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/* Copyright (C) 2021 RDK Management.  All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+* PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS. OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+* OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 
@@ -37,13 +42,44 @@ std::recursive_mutex _lock;
 #else // USE_LIBC_SCOPED_LOCK
 ScopedMutex::ScopedMutex(const char* strFN) 
 : _strFN(strFN)
+, _bMutexAcquired(false)
 {
     if(!_bMutexInit) {
         InitMutex(&_lock);
     }
     
     if(_bMutexInit) {
-        pthread_mutex_lock(&_lock);
+        if(pthread_mutex_lock(&_lock) == 0) {
+            _bMutexAcquired = true;
+        }
+    }
+    else {
+        LOG(eError, "Mutex was not initialized for %s\n", _strFN);
+    }
+}
+ScopedMutex::ScopedMutex(const char* strFN, uint32_t timeoutMS)
+: _strFN(strFN)
+, _bMutexAcquired(false)
+{
+    uint32_t nCount = 0;
+
+    if(!_bMutexInit) {
+        InitMutex(&_lock);
+    }
+    
+    if(_bMutexInit) {
+        while(nCount < timeoutMS) {
+            if(pthread_mutex_trylock(&_lock) != 0) {
+                // Mutex not acquired
+                usleep(1000); //1 ms
+                nCount++;
+            }
+            else {
+                // mutex acquired
+                _bMutexAcquired = true;
+                break;
+            }
+        }
     }
     else {
         LOG(eError, "Mutex was not initialized for %s\n", _strFN);
@@ -51,7 +87,10 @@ ScopedMutex::ScopedMutex(const char* strFN)
 }
 ScopedMutex::~ScopedMutex() 
 {
-    pthread_mutex_unlock(&_lock);
+    if(_bMutexAcquired) {
+        pthread_mutex_unlock(&_lock);
+        _bMutexAcquired = false;
+    }
 }
 void ScopedMutex::InitMutex(pthread_mutex_t* pLock)
 {
--- gst_svp_scopedlock.h
+++ gst_svp_scopedlock.h
@@ -1,25 +1,33 @@
-/*
- * If not stated otherwise in this file or this component's LICENSE file the
- * following copyright and licenses apply:
- *
- * Copyright 2021 RDK Management
- *
- * Licensed under the Apache License, Version 2.0 (the License);
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/* Copyright (C) 2021 RDK Management.  All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+* PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS. OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+* OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 
 #ifndef __GST_SVP_SCOPEDLOCK_H__
 #define __GST_SVP_SCOPEDLOCK_H__
+
+#include <unistd.h>     // For usleep()
+#include "gst_svp_logging.h"
 
 #ifdef USE_LIBC_SCOPED_LOCK
 #include <mutex>
@@ -30,21 +38,54 @@
 #ifdef USE_LIBC_SCOPED_LOCK
 extern std::recursive_mutex _lock;
 #define SCOPED_LOCK()     std::lock_guard<std::recursive_mutex> lock(_lock)
+#define SCOPED_TRY_LOCK(timeoutMS, failRetVal)  \
+    int     nTryCount = 0;                      \
+    bool    bLockAcquired = false;              \
+    while(nTryCount < timeoutMS) {              \
+        bLockAcquired = _lock.try_lock();       \
+        if(bLockAcquired == false) {            \
+            nTryCount++;                        \
+            usleep(1000); /* 1ms */             \
+        }                                       \
+        else {                                  \
+            break;                              \
+        }                                       \
+    }                                           \
+    if(bLockAcquired != true) {                 \
+        LOG(eError, "%s failed to acquire mutex after %d ms\n", \
+                    __FUNCTION__, timeoutMS);   \
+        return failRetVal;                      \
+    }                                           \
+    SCOPED_LOCK();                              \
+    _lock.unlock()
 #else
 class ScopedMutex
 {
 public:
     ScopedMutex(const char* strFN);
+    ScopedMutex(const char* strFN, uint32_t timeoutMS);
     ~ScopedMutex();
+
+    bool IsMutexLocked() { return _bMutexAcquired; };
+
 private:
     void InitMutex(pthread_mutex_t* pLock);
 
-    const char* _strFN;
-    static bool _bMutexInit;
+    const char*             _strFN;
+    bool                    _bMutexAcquired;
 
-    static pthread_mutex_t _lock;
+    static bool             _bMutexInit;
+    static pthread_mutex_t  _lock;
 };
 
-#define SCOPED_LOCK()     ScopedMutex lock(__FUNCTION__)
+#define SCOPED_LOCK()                               ScopedMutex lock(__FUNCTION__)
+#define SCOPED_TRY_LOCK(timeoutMS, failRetVal)                  \
+    ScopedMutex lock(__FUNCTION__, timeoutMS);                  \
+    if(!lock.IsMutexLocked()) {                                 \
+        LOG(eError, "%s failed to acquire mutex after %d ms\n", \
+                    __FUNCTION__, timeoutMS);                   \
+        return failRetVal;                                      \
+    }
 #endif
+
 #endif //__GST_SVP_SCOPEDLOCK_H__
